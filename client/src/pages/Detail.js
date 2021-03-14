6fe12601d303b326cd90b82a5d6507dd12a83cd9
JamesCostello-dev <jcotab@gmail.com>
2021-03-14 14:52:02 -0700

    add store, update reducers and actions, remove context and replace with redux useSelector and useDisptach

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
 import Cart from "../components/Cart";
-import { useStoreContext } from "../utils/GlobalState";
+import { useDispatch, useSelector } from "react-redux";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +15,9 @@ import { idbPromise } from "../utils/helpers";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
@@ -28,7 +30,7 @@ function Detail() {
     // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } 
+    }
     // retrieved from server
     else if (data) {
       dispatch({
@@ -103,8 +105,8 @@ function Detail() {
             <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button 
-              disabled={!cart.find(p => p._id === currentProduct._id)} 
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
               onClick={removeFromCart}
             >
               Remove from Cart
